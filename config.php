189cb8fbb02a02c8cbeb0057a64a91d096a0173f
Robertas <galaja@inbox.lt>
2022-04-06 18:51:12 +0300

    created config file

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+$database = mysqli_connect('127.0.0.1', 'root', '', 'shop_warehouse');
+
+if (!$database) {
+    die("connection failed: " . mysqli_connect_error());
+}
+
+$page = $_REQUEST['page'] ?? null;
+
+function isLoged(): bool
+{
+    if (isset($_SESSION['email'])) {
+        return true;
+    } else {
+        return false;
+    }
+}

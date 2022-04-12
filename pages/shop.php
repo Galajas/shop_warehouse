f1d291765cafd2167c70cb28cbbb62164721f8bf
Robertas <galaja@inbox.lt>
2022-04-12 22:04:13 +0300

    started shop margin saving

--- pages/shop.php
+++ pages/shop.php
@@ -1,4 +1,12 @@
 <h1>Parduotuves valdymas</h1>
+
+<style>
+    .table th, .table td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding: 10px;
+    }
+</style>
 <?php
 
 if (isLoged()) { ?>
@@ -11,8 +19,9 @@ if (isLoged()) { ?>
 
     ?>
     <form action="index.php" method="get">
-        <table>
-            <td>Parduotuves pavadinimas</td>
+        <table class="table">
+            <tr>
+            <td>Parduotuves pasirinkimas</td>
             <td>
                 <input type="hidden" name="page" value="shop">
                 <select name="shopId">
@@ -28,15 +37,54 @@ if (isLoged()) { ?>
                     ?>
                 </select>
             </td>
-            <td>
-                <button type="submit">Pasirinkti</button>
-            </td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center">
+                    <button style="width: 200px; height: 30px" type="submit">Pasirinkti</button>
+                </td>
+            </tr>
         </table>
     </form>
     <?php
 
     if (isset($_GET['shopId'])) {
-        if (in_array($_GET['shopId'], array_column($get_shops, 'id'))) {
+        if (in_array($_GET['shopId'], array_column($get_shops, 'id'))) {?>
+
+
+            <h3>Marzos pridejimas</h3>
+            <form action="index.php?page=shop&shopId=<?php echo $_GET['shopId']?>" method="post">
+                <table class="table">
+                    <tr>
+                        <td>
+                            Maržos kategorija
+                        </td>
+                        <td>
+                            <select name="margin_size">
+                                <option>
+                                    kategorija
+                                </option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Marzos dydis
+                        </td>
+                        <td>
+                            <input type="number" name="margin_size">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align: center">
+                            <button style="width: 200px; height: 30px" type="submit">Prideti</button>
+                        </td>
+                    </tr>
+
+                </table>
+            </form>
+
+
+            <?php
         } else {
             header('Location: index.php?page=shop');
         }

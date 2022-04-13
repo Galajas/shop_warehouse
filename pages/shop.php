c656cf060bb7676a5fd2867eaf1fe3efe2337046
Robertas <galaja@inbox.lt>
2022-04-13 22:10:37 +0300

    ended margin creation and display

--- pages/shop.php
+++ pages/shop.php
@@ -9,6 +9,7 @@
 </style>
 <?php
 
+
 if (isLoged()) { ?>
     <h3>Pasirinkite parduotuve</h3>
 
@@ -16,13 +17,15 @@ if (isLoged()) { ?>
     $get_shops = mysqli_query($database, 'select * from shop');
     $get_shops = mysqli_fetch_all($get_shops, MYSQLI_ASSOC);
 
+    $get_margin = mysqli_query($database, "select * from shop_margin");
+    $get_margin = mysqli_fetch_all($get_margin, MYSQLI_ASSOC);
+
+
     if (isset($_POST['margin_size'])) {
         $shop_id = $_GET['shopId'];
         $product_category = $_POST['product_category'];
         $margin_size = $_POST['margin_size'];
 
-        $get_margin_type = mysqli_query($database, "select * from shop_margin");
-        $get_margin_type = mysqli_fetch_all($get_margin_type, MYSQLI_ASSOC);
 
         $errors = [];
 
@@ -34,13 +37,12 @@ if (isLoged()) { ?>
             $errors[] = 'marza negali buti mazesne nei 1';
         }
 
-        if (!in_array($product_category, array_column($get_margin_type, 'margin_type'))) {
+        if (!in_array($product_category, array_column($get_margin, 'margin_type'))) {
             $errors[] = 'Pasirinkta neteisinga kategorija';
         }
 
         if (empty($errors)) {
-
-            if (in_array($product_category, array_column($get_margin_type, 'margin_type')) && in_array($shop_id, array_column($get_margin_type, 'shop_id'))) {
+            if (in_array($product_category, array_column($get_margin, 'margin_type')) && in_array($shop_id, array_column($get_margin, 'shop_id'))) {
                 $update_margin = mysqli_query($database, "update shop_margin set margin_size = '$margin_size' where margin_type = '$product_category' and shop_id = '$shop_id'");
                 echo 'Marza atnaujinta';
             } else {
@@ -58,7 +60,6 @@ if (isLoged()) { ?>
             }
         }
     }
-
     ?>
     <form action="index.php" method="get">
         <table class="table">
@@ -90,8 +91,12 @@ if (isLoged()) { ?>
     <?php
 
     if (isset($_GET['shopId'])) {
-        if (in_array($_GET['shopId'], array_column($get_shops, 'id'))) { ?>
-
+        if (in_array($_GET['shopId'], array_column($get_shops, 'id'))) {
+            $shop_id = $_GET['shopId'];
+            $get_margin_via_shop = mysqli_query($database, "select * from shop_margin where shop_id = '$shop_id'");
+            $get_margin_via_shop = mysqli_fetch_all($get_margin_via_shop, MYSQLI_ASSOC);
+            ?>
+            <h2><?php echo mysqli_fetch_row(mysqli_query($database, 'select shop_name from shop where id = ' . "$shop_id" . ' '))[0]; ?></h2>
             <h3>Marzos pridejimas</h3>
             <div style="display: flex">
                 <form style="margin-bottom: 0px" action="index.php?page=shop&shopId=<?php echo $_GET['shopId'] ?>"
@@ -129,29 +134,40 @@ if (isLoged()) { ?>
                                 <button style="width: 200px; height: 30px" type="submit">Prideti</button>
                             </td>
                         </tr>
-
                     </table>
                 </form>
-
                 <table class="table">
                     <tr>
                         <th>
                             Marzos kategorija
                         </th>
-                        <td>
-                            Darzoves
-                        </td>
+                        <?php
+                        foreach ($get_margin_via_shop as $margin) { ?>
+                            <td>
+                                <?php
+                                if (array_key_exists($margin['margin_type'], mutateArray(MARGIN_CATEGORIES))) {
+                                    echo mutateArray(MARGIN_CATEGORIES)[$margin['margin_type']];
+                                }
+                                ?>
+                            </td>
+                        <?php } ?>
                     </tr>
                     <tr>
                         <th>
                             Marzos dydis
                         </th>
-                        <td>
-                            1.5
-                        </td>
+                        <?php
+                        foreach ($get_margin_via_shop as $margin) { ?>
+                            <td>
+                                <?php
+                                echo $margin['margin_size'];
+                                ?>
+                            </td>
+                        <?php } ?>
                     </tr>
                 </table>
             </div>
+            
 
 
             <?php
